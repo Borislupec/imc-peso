d0d6a4034c922588c14e57cc8e5a18cb0aeb29ed
RYAN LEONARDO FERNANDES MARTINS <00001073887352sp@aluno.educacao.sp.gov.br>
2024-06-05 14:36:49 -0300

    IMC

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function calcular(){
-    var fPeso = parseFloat(document.getElementById("peso"))
-    var fAltura = parseFloat(document.getElementById("altura"))
+    var fPeso = parseFloat(document.getElementById("peso").value)
+    console.log(fPeso)
+    var fAltura = parseFloat(document.getElementById("altura").value)
     var fIMC = fPeso/(fAltura*fAltura)
+    document.getElementById("imc").innerHTML = fIMC
+    if(fIMC<18.5){
+        document.getElementById("STATUS").innerHTML = "STATUS: BAIXO PESO"  
+    } else if(fIMC<=24.9){
+        document.getElementById("STATUS").innerHTML = "STATUS: PESO IDEAL"
+    } else if(fIMC<=29.9){
+        document.getElementById("STATUS").innerHTML = "STATUS: SOBREPESO"
+    } else if(fIMC<=34.9){
+        document.getElementById("STATUS").innerHTML = "STATUS: OBESIDADE 1"
+    } else if(fIMC<=39.9){
+        document.getElementById("STATUS").innerHTML = "STATUS: OBESIDADE 2"
+    }else{
+        document.getElementById("STATUS").innerHTML = "STATUS: OBESIDADE MORBIDA"
+    }
 }
